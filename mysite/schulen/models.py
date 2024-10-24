3fbe0a1453caa17103e1bf57e2095801ce225854
Mohammad Ahmed <ardyop7@gmail.com>
2024-10-24 16:10:14 +0000

    init many things

--- mysite/schulen/models.py
+++ mysite/schulen/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from django.utils import timezone #-> Imported to enable the timezone field.
 
 # Create your models here.
 class AnimeVariety(models.Model):
-    
+    Anime_types = [
+        ('JK', 'JUJUTSUKAISEN'),
+        ('NR', 'NARUTO'),
+        ('AO', 'ATTACKONTITAN'),
+    ]
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='animeimg/')
+    date_added = models.DateTimeField(default=timezone.now)
+    types = models.CharField(max_length=2, choices=Anime_types)
+
+    def __str__(self):
+        return self.name
+
--- mysite/schulen/views.py
+++ mysite/schulen/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import AnimeVariety
 
 def schulen(request):
-    return render(request, "schulen/schulen.html")
+    animes = AnimeVariety.objects.all() # <- this will return an array.
+    return render(request, "schulen/schulen.html", {"animes": animes})
+
